26eb8fc7119fcc4825ba5037dc5aacc03fd7ffb1
Matt <matt24ray@gmail.com>
2016-05-20 22:06:35 -0500

    added event to api sucessfully

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,7 +1,7 @@
 class API::EventsController < ApplicationController
-
-
   before_filter :set_access_control_headers
+  skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user!
 
    def set_access_control_headers
       headers['Access-Control-Allow-Origin'] = '*'
@@ -9,10 +9,21 @@ class API::EventsController < ApplicationController
       headers['Access-Control-Allow-Headers'] = 'Content-Type'
    end
 
-   skip_before_action :verify_authenticity_token
-
    def create
-      registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
+      @registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
+      @event = Event.new(event_params)
+      @event.registered_application = @registered_application
+
+      # if @registered_application.nil?
+      #   render json: "Unregistered application", status: :unprocessable_entity
+      # end
+
+      if @event.save
+        render json: @event, status: :created
+      else
+        render json: {errors: @event.errors}, status: :unprocessable_entity
+      end
+
 
    end
 
